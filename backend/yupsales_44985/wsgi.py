610f90254d9bee2fc3267d12099b09169fa1b1db
Crowdbotics <team@crowdbotics.com>
2023-12-13 05:24:19 +0000

    Initial commit

---
+++ backend/yupsales_44985/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for yupsales_44985 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "yupsales_44985.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yupsales-44985",
+  url: "https://yupsales-44985.botics.co",
+  api: "https://yupsales-44985.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
